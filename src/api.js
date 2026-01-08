f11887de8c3fa9c67ff495cc2ecd377d13d98d7a
Josephine Obodai <joseikwart@gmail.com>
2026-01-08 07:37:03 +0000

    add api.js for data fetching

---
+++ src/api.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const instance = axios.create({ baseURL: "https://nc-news-vafy.onrender.com" });
+
+export const getArticles = () => {
+  return instance.get("/api/articles");
+};
+
+export const getArticleById = (article_id) => {
+  return instance.get(`/api/articles/${article_id}`);
+};
